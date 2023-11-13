90ebc8106d478fc390a46a8171e15eaad224d36e
MapaRuiz <150563352+MapaRuiz@users.noreply.github.com>
2023-11-12 23:22:34 -0500

    Update b.asm

--- b.asm
+++ b.asm
@@ -1,8 +1,8 @@
 
 variableA: 0b00000000
-Q: 0b00000001 ; Multiplicador
+Q: 0b01100111 ; Multiplicador
 Q_1: 0b00000000
-M: 0b00000001; Multiplicando
+M: 0b10000111; Multiplicando
 count: 0x8
 
 Bmenos: 0b00000001
